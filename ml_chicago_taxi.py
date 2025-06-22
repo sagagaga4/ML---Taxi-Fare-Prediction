ec0c3e322f909705f54f6ffca99a7e557e39cb4c
‪SAGI <57183982+sagagaga4@users.noreply.github.com>
2025-06-22 18:21:49 +0300

    Add files via upload

---
+++ ml_chicago_taxi.py
@@ -0,0 +1,284 @@
+#@title Code - Load dependencies
+
+#general
+import io
+
+# data
+import numpy as np
+import pandas as pd
+
+# machine learning
+import keras
+import matplotlib.pyplot as plt
+
+# data visualization
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import seaborn as sns
+# data preprocessing
+
+chicago_taxi_dataset = pd.read_csv("chicago_taxi_train.csv")
+
+# Updates dataframe to use specific columns.
+training_df = chicago_taxi_dataset[['TRIP_MILES', 'TRIP_SECONDS', 'FARE', 'COMPANY', 'PAYMENT_TYPE', 'TIP_RATE']]
+
+print('Read dataset completed successfully.')
+print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
+training_df.head(200)
+
+print('Total number of rows: {0}\n\n'.format(len(training_df.index)))
+training_df.describe(include='all')
+
+#@title Double-click or run to view answers about dataset statistics
+
+answer = '''
+What is the maximum fare? 				              Answer: $159.25
+What is the mean distance across all trips? 		Answer: 8.2895 miles
+How many cab companies are in the dataset? 		  Answer: 31
+What is the most frequent payment type? 		    Answer: Credit Card
+Are any features missing data? 				          Answer: No
+'''
+
+# What is the maximum fare?
+max_fare = training_df['FARE'].max()
+print("What is the maximum fare? \t\t\t\tAnswer: ${fare:.2f}".format(fare = max_fare))
+
+# What is the mean distance across all trips?
+mean_distance = training_df['TRIP_MILES'].mean()
+print("What is the mean distance across all trips? \t\tAnswer: {mean:.4f} miles".format(mean = mean_distance))
+
+# How many cab companies are in the dataset?
+num_unique_companies =  training_df['COMPANY'].nunique()
+print("How many cab companies are in the dataset? \t\tAnswer: {number}".format(number = num_unique_companies))
+
+# What is the most frequent payment type?
+most_freq_payment_type = training_df['PAYMENT_TYPE'].value_counts().idxmax()
+print("What is the most frequent payment type? \t\tAnswer: {type}".format(type = most_freq_payment_type))
+
+# Are any features missing data?
+missing_values = training_df.isnull().sum().sum()
+print("Are any features missing data? \t\t\t\tAnswer:", "No" if missing_values == 0 else "Yes")
+
+#@title Code - View correlation matrix
+training_df.corr(numeric_only = True)
+
+#@title Double-click to view answers about the correlation matrix
+
+# Which feature correlates most strongly to the label FARE?
+# ---------------------------------------------------------
+answer = '''
+The feature with the strongest correlation to the FARE is TRIP_MILES.
+As you might expect, TRIP_MILES looks like a good feature to start with to train
+the model. Also, notice that the feature TRIP_SECONDS has a strong correlation
+with fare too.
+'''
+print(answer)
+
+
+# Which feature correlates least strongly to the label FARE?
+# -----------------------------------------------------------
+answer = '''The feature with the weakest correlation to the FARE is TIP_RATE.'''
+print(answer)
+
+#@title Code - View pairplot
+sns.pairplot(training_df, x_vars=["FARE", "TRIP_MILES", "TRIP_SECONDS"], y_vars=["FARE", "TRIP_MILES", "TRIP_SECONDS"])
+
+#@title Define plotting functions
+
+def make_plots(df, feature_names, label_name, model_output, sample_size=200):
+
+  random_sample = df.sample(n=sample_size).copy()
+  random_sample.reset_index()
+  weights, bias, epochs, rmse = model_output
+
+  is_2d_plot = len(feature_names) == 1
+  model_plot_type = "scatter" if is_2d_plot else "surface"
+  fig = make_subplots(rows=1, cols=2,
+                      subplot_titles=("Loss Curve", "Model Plot"),
+                      specs=[[{"type": "scatter"}, {"type": model_plot_type}]])
+
+  plot_data(random_sample, feature_names, label_name, fig)
+  plot_model(random_sample, feature_names, weights, bias, fig)
+  plot_loss_curve(epochs, rmse, fig)
+
+  fig.show()
+  return
+
+def plot_loss_curve(epochs, rmse, fig):
+  curve = px.line(x=epochs, y=rmse)
+  curve.update_traces(line_color='#ff0000', line_width=3)
+
+  fig.append_trace(curve.data[0], row=1, col=1)
+  fig.update_xaxes(title_text="Epoch", row=1, col=1)
+  fig.update_yaxes(title_text="Root Mean Squared Error", row=1, col=1, range=[rmse.min()*0.8, rmse.max()])
+
+  return
+
+def plot_data(df, features, label, fig):
+  if len(features) == 1:
+    scatter = px.scatter(df, x=features[0], y=label)
+  else:
+    scatter = px.scatter_3d(df, x=features[0], y=features[1], z=label)
+
+  fig.append_trace(scatter.data[0], row=1, col=2)
+  if len(features) == 1:
+    fig.update_xaxes(title_text=features[0], row=1, col=2)
+    fig.update_yaxes(title_text=label, row=1, col=2)
+  else:
+    fig.update_layout(scene1=dict(xaxis_title=features[0], yaxis_title=features[1], zaxis_title=label))
+
+  return
+
+def plot_model(df, features, weights, bias, fig):
+  df['FARE_PREDICTED'] = bias[0]
+
+  for index, feature in enumerate(features):
+    df['FARE_PREDICTED'] = df['FARE_PREDICTED'] + weights[index][0] * df[feature]
+
+  if len(features) == 1:
+    model = px.line(df, x=features[0], y='FARE_PREDICTED')
+    model.update_traces(line_color='#ff0000', line_width=3)
+  else:
+    z_name, y_name = "FARE_PREDICTED", features[1]
+    z = [df[z_name].min(), (df[z_name].max() - df[z_name].min()) / 2, df[z_name].max()]
+    y = [df[y_name].min(), (df[y_name].max() - df[y_name].min()) / 2, df[y_name].max()]
+    x = []
+    for i in range(len(y)):
+      x.append((z[i] - weights[1][0] * y[i] - bias[0]) / weights[0][0])
+
+    plane=pd.DataFrame({'x':x, 'y':y, 'z':[z] * 3})
+
+    light_yellow = [[0, '#89CFF0'], [1, '#FFDB58']]
+    model = go.Figure(data=go.Surface(x=plane['x'], y=plane['y'], z=plane['z'],
+                                      colorscale=light_yellow))
+
+  fig.add_trace(model.data[0], row=1, col=2)
+
+  return
+
+def model_info(feature_names, label_name, model_output):
+  weights = model_output[0]
+  bias = model_output[1]
+
+  nl = "\n"
+  header = "-" * 80
+  banner = header + nl + "|" + "MODEL INFO".center(78) + "|" + nl + header
+
+  info = ""
+  equation = label_name + " = "
+
+  for index, feature in enumerate(feature_names):
+    info = info + "Weight for feature[{}]: {:.3f}\n".format(feature, weights[index][0])
+    equation = equation + "{:.3f} * {} + ".format(weights[index][0], feature)
+
+  info = info + "Bias: {:.3f}\n".format(bias[0])
+  equation = equation + "{:.3f}\n".format(bias[0])
+
+  return banner + nl + info + nl + equation
+
+print("SUCCESS: defining plotting functions complete.")
+
+#@title Code - Define ML functions
+
+def build_model(my_learning_rate, num_features):
+  """Create and compile a simple linear regression model."""
+  # Describe the topography of the model.
+  # The topography of a simple linear regression model
+  # is a single node in a single layer.
+  inputs = keras.Input(shape=(num_features,))
+  outputs = keras.layers.Dense(units=1)(inputs)
+  model = keras.Model(inputs=inputs, outputs=outputs)
+
+  # Compile the model topography into code that Keras can efficiently
+  # execute. Configure training to minimize the model's mean squared error.
+  model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=my_learning_rate),
+                loss="mean_squared_error",
+                metrics=[keras.metrics.RootMeanSquaredError()])
+
+  return model
+
+
+def train_model(model, features, label, epochs, batch_size):
+  """Train the model by feeding it data."""
+
+  # Feed the model the feature and the label.
+  # The model will train for the specified number of epochs.
+  history = model.fit(x=features,
+                      y=label,
+                      batch_size=batch_size,
+                      epochs=epochs)
+
+  # Gather the trained model's weight and bias.
+  trained_weight = model.get_weights()[0]
+  trained_bias = model.get_weights()[1]
+
+  # The list of epochs is stored separately from the rest of history.
+  epochs = history.epoch
+
+  # Isolate the error for each epoch.
+  hist = pd.DataFrame(history.history)
+
+  # To track the progression of training, we're going to take a snapshot
+  # of the model's root mean squared error at each epoch.
+  rmse = hist["root_mean_squared_error"]
+
+  return trained_weight, trained_bias, epochs, rmse
+
+
+def run_experiment(df, feature_names, label_name, learning_rate, epochs, batch_size):
+
+  print('INFO: starting training experiment with features={} and label={}\n'.format(feature_names, label_name))
+
+  num_features = len(feature_names)
+
+  features = df.loc[:, feature_names].values
+  label = df[label_name].values
+
+  model = build_model(learning_rate, num_features)
+  model_output = train_model(model, features, label, epochs, batch_size)
+
+  print('\nSUCCESS: training experiment complete\n')
+  print('{}'.format(model_info(feature_names, label_name, model_output)))
+  make_plots(df, feature_names, label_name, model_output)
+
+  return model
+
+print("SUCCESS: defining linear regression functions complete.")
+
+#@title Code - Experiment 1
+
+# The following variables are the hyperparameters.
+learning_rate = 0.001
+epochs = 20
+batch_size = 50
+
+# Specify the feature and the label.
+features = ['TRIP_MILES']
+label = 'FARE'
+
+model_1 = run_experiment(training_df, features, label, learning_rate, epochs, batch_size)
+
+#@title Double-click to view answers for training model with one feature
+
+# How many epochs did it take to converge on the final model?
+# -----------------------------------------------------------------------------
+answer = """
+Use the loss curve to see where the loss begins to level off during training.
+
+With this set of hyperparameters:
+
+  learning_rate = 0.001
+  epochs = 20
+  batch_size = 50
+
+it takes about 5 epochs for the training run to converge to the final model.
+"""
+print(answer)
+
+# How well does the model fit the sample data?
+# -----------------------------------------------------------------------------
+answer = '''
+It appears from the model plot that the model fits the sample data fairly well.
+'''
+print(answer)
